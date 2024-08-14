b59812217226df6a37f7c3678d425c2bee9e1986
Cristobal Torres <torresalvarezcristobal@gmail.com>
2024-08-13 21:27:40 -0400

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+        <Header />
+        <main className="">{children}</main>
+        <Footer />
+    </div>
+  );
+}
