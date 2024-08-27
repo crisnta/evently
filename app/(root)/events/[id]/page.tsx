f5a0c562f6c6512d2cc3073dd1e22aeb4abc1bc4
Cristobal Torres <torresalvarezcristobal@gmail.com>
2024-08-27 15:20:05 -0400

    update form component

---
+++ app/(root)/events/[id]/page.tsx
@@ -0,0 +1,15 @@
+import { getEventById } from '@/lib/actions/event.action'
+import { SearchParamProps } from '@/types'
+import React from 'react'
+
+const EventDetails = async ({ params: { id }}: SearchParamProps) => {
+
+  const event = await getEventById(id)
+  console.log(event)
+  
+  return (
+    <div></div>
+  )
+}
+
+export default EventDetails
